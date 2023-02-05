f61ca6002ea40b0f232a9c30571d124c04533184
Gunnar Funderburk <alfredpysemail@gmail.com>
2023-02-05 12:35:11 -0500

    Update to conform to new API

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 ###############################
 
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='!',intents=intents)
 #--------------------EVENTS--------------------------
